177e008371e9b7d318479b254570571646996cd3
michloud <chinoah96@gmail.com>
2019-11-04 16:24:23 +0000

    write test for adding item to database through form create

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -3,8 +3,18 @@ class GramsController < ApplicationController
   end
 
   def new
-    @gram = Gram.new
+    @gram = Gram.new{"Hello!"}
   end
 
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
 
 end
