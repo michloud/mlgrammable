1edf9d637f571157ff2d2c9f6b8eff2114e9855a
michloud <chinoah96@gmail.com>
2019-11-11 21:46:21 +0000

    write tests to add comments to grams;add comments to routes file;def create in Comments controller,add relationships to user,grams and comments models,add autentication to comments controller

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -21,7 +21,7 @@ class GramsController < ApplicationController
 
   def show
     @gram = Gram.find_by_id(params[:id])
-    return render_not_found if @gram.blank? 
+    return render_not_found if @gram.blank?
   end
 
   def edit
